cffa4ba776b634fd6790aae73165612a1faad750
MDCoetzee <coetzeem211@gmail.com>
2023-05-17 11:20:02 +0200

    I have created the RecipeClass to allow the user to enter multiple recipes. This class serves as a container for storing recipe objects, each representing a distinct recipe with its own steps and ingredients.

---
+++ POE_PROG6221_ST10023767_GR01/Classes/RecipeClass.cs
@@ -0,0 +1,27 @@
+﻿#region
+// Mikayle Coetzee
+// ST10023767
+// PROG6221 POE 2023
+// Part 1 
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE_PROG6221_ST10023767_GR01
+{
+    public class RecipeClass
+    {
+        //・♫-------------------------------------------------------------------------------------------------♫・//
+        /// <summary>
+        /// Default constructor for StepClass.
+        /// </summary>
+        public RecipeClass()
+        {
+
+        }
+    }
+}//★---♫:;;;: ♫ ♬:;;;:♬ ♫:;;;: ♫ ♬:;;;:♬ ♫---★・。。END OF FILE 。。・★---♫ ♬:;;;:♬ ♫:;;;: ♫ ♬:;;;:♬ ♫:;;;: ♫---★//
